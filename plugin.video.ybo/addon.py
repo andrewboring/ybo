b398d72dd1cc295eff69faaad8d68676d03a7f55
Andrew Boring <andrew@andrewboring.com>
2020-04-05 17:13:51 -0400

    working HTTP listing

---
+++ plugin.video.ybo/addon.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import sys
+import os
+import requests
+#from urllib.parse import quote
+#import urllib.parse
+import xbmcgui
+import xbmcplugin
+
+
+source_url = 'https://storage.api.eriscape.com/v1/AUTH_andrew/kodi/'
+r = requests.get(source_url)
+items = r.text
+
+addon_handle = int(sys.argv[1])
+xbmcplugin.setContent(addon_handle, 'movies')
+
+#for item in items.splitlines():
+#    name = os.path.splitext(item)[0]
+#    link = source_url + urllib.parse.quote(item)
+#    print("Movie: " + name)
+#    print("Link: " + link)
+
+for item in items.splitlines():
+    link = source_url + item
+    name = os.path.splitext(item)[0]
+    li = xbmcgui.ListItem(name, iconImage='DefaultVideo.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=link, listitem=li)
+
+xbmcplugin.endOfDirectory(addon_handle)
